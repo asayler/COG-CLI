861e9ea704c9055705ab380980222e312dfba011
Andy Sayler <andy.sayler@gmail.com>
2015-09-10 22:02:55 -0600

    Seperted client calls into seperte library

--- cli.py
+++ cli.py
@@ -6,13 +6,9 @@ import json
 import requests
 import click
 
+import client
+
 _EP_TOKENS = 'tokens'
-_EP_ASSIGNMENTS = 'assignments'
-_KEY_ASSIGNMENTS = 'assignments'
-_EP_TESTS = 'tests'
-_KEY_TESTS = 'tests'
-_EP_FILES = 'files'
-_KEY_FILES = 'files'
 
 def _debug_dump(r):
 
@@ -57,103 +53,6 @@ def _auth(obj):
 
     return auth
 
-def _assignment_create(url, auth, asn_name, asn_env):
-
-    endpoint = "{:s}/{:s}/".format(url, _EP_ASSIGNMENTS)
-    d = {'name': asn_name, 'env': asn_env}
-    dj = json.dumps(d)
-    r = requests.post(endpoint, auth=auth, data=dj)
-    r.raise_for_status()
-    asn_list = r.json()[_KEY_ASSIGNMENTS]
-    return asn_list
-
-def _assignment_list(url, auth):
-
-    endpoint = "{:s}/{:s}/".format(url, _EP_ASSIGNMENTS)
-    r = requests.get(endpoint, auth=auth)
-    r.raise_for_status()
-    asn_list = r.json()[_KEY_ASSIGNMENTS]
-    return asn_list
-
-def _assignment_show(url, auth, uid):
-
-    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_ASSIGNMENTS, uid)
-    r = requests.get(endpoint, auth=auth)
-    r.raise_for_status()
-    asn = r.json()[uid]
-    return asn
-
-def _assignment_test_create(url, auth, asn_uid, tst_name, tst_tester, tst_maxscore):
-
-    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_ASSIGNMENTS, asn_uid, _EP_TESTS)
-    d = {"name": tst_name, "tester": tst_tester, "maxscore": tst_maxscore}
-    dj = json.dumps(d)
-    r = requests.post(endpoint, auth=auth, data=dj)
-    r.raise_for_status()
-    tst_list = r.json()[_KEY_TESTS]
-    return tst_list
-
-def _test_file_add(url, auth, tst_uid, fle_uids):
-
-    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
-    d = {_KEY_FILES: fle_uids}
-    dj = json.dumps(d)
-    r = requests.put(endpoint, auth=auth, data=dj)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
-
-def _test_file_remove(url, auth, tst_uid, fle_uids):
-
-    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
-    d = {_KEY_FILES: fle_uids}
-    dj = json.dumps(d)
-    r = requests.delete(endpoint, auth=auth, data=dj)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
-
-def _file_create(url, auth, path, extract=False):
-
-    if extract:
-        key = 'extract'
-    else:
-        key = 'file'
-
-    endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
-    files = {key: path}
-    r = requests.post(endpoint, files=files, auth=auth)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
-
-def _file_list(url, auth, tst_uid=None):
-
-    if tst_uid:
-        endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
-    else:
-        endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
-    r = requests.get(endpoint, auth=auth)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
-
-def _file_show(url, auth, uid):
-
-    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
-    r = requests.get(endpoint, auth=auth)
-    r.raise_for_status()
-    fle = r.json()[uid]
-    return fle
-
-def _file_delete(url, auth, uid):
-
-    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
-    r = requests.delete(endpoint, auth=auth)
-    r.raise_for_status()
-    fle = r.json()[uid]
-    return fle
-
 @click.group()
 @click.option('--url', default=None, prompt=True, help='API URL')
 @click.option('--username', default=None, help='API Username')
@@ -185,7 +84,7 @@ def assignment_create(obj, name, env):
         obj['auth'] = _auth(obj)
 
     click.echo("Creating assignment...")
-    asn_list = _assignment_create(obj['url'], obj['auth'], name, env)
+    asn_list = client.assignment_create(obj['url'], obj['auth'], name, env)
     click.echo("Created assignments:\n {}".format(asn_list))
 
 @assignment.command(name='list')
@@ -196,7 +95,7 @@ def assignment_list(obj):
         obj['auth'] = _auth(obj)
 
     click.echo("Listing assignments")
-    asn_list = _assignment_list(obj['url'], obj['auth'])
+    asn_list = client.assignment_list(obj['url'], obj['auth'])
     click.echo("Assignments:\n {}".format(asn_list))
 
 @assignment.command(name='show')
@@ -208,7 +107,7 @@ def assignment_show(obj, uid):
         obj['auth'] = _auth(obj)
 
     click.echo("Showing assignment...")
-    asn = _assignment_show(obj['url'], obj['auth'], uid)
+    asn = client.assignment_show(obj['url'], obj['auth'], uid)
     click.echo("Assignment '{}':\n {}".format(uid, asn))
 
 @cli.group()
@@ -227,7 +126,7 @@ def test_create(obj, asn_uid, name, tester, maxscore):
         obj['auth'] = _auth(obj)
 
     click.echo("Creating test...")
-    tst_list = _assignment_test_create(obj['url'], obj['auth'], asn_uid, name, tester, maxscore)
+    tst_list = client.assignment_test_create(obj['url'], obj['auth'], asn_uid, name, tester, maxscore)
     click.echo("Created tests:\n {}".format(tst_list))
 
 @cli.group()
@@ -244,7 +143,7 @@ def file_create(obj, path, extract):
         obj['auth'] = _auth(obj)
 
     click.echo("Creating file...")
-    fle_list = _file_create(obj['url'], obj['auth'], path, extract)
+    fle_list = client.file_create(obj['url'], obj['auth'], path, extract)
     click.echo("Files:\n {}".format(fle_list))
 
 @file.command(name='list')
@@ -256,7 +155,7 @@ def file_list(obj, tst_uid):
         obj['auth'] = _auth(obj)
 
     click.echo("Listing files...")
-    fle_list = _file_list(obj['url'], obj['auth'], tst_uid=tst_uid)
+    fle_list = client.file_list(obj['url'], obj['auth'], tst_uid=tst_uid)
     click.echo("Files:\n {}".format(fle_list))
 
 @file.command(name='show')
@@ -268,7 +167,7 @@ def file_show(obj, uid):
         obj['auth'] = _auth(obj)
 
     click.echo("Showing file...")
-    fle = _file_show(obj['url'], obj['auth'], uid)
+    fle = client.file_show(obj['url'], obj['auth'], uid)
     click.echo("File '{}':\n {}".format(uid, fle))
 
 @file.command(name='show')
@@ -280,7 +179,7 @@ def file_delete(obj, uid):
         obj['auth'] = _auth(obj)
 
     click.echo("Deleting file...")
-    fle = _file_delete(obj['url'], obj['auth'], uid)
+    fle = client.file_delete(obj['url'], obj['auth'], uid)
     click.echo("Deleted file '{}':\n {}".format(uid, fle))
 
 @cli.group()
@@ -298,24 +197,24 @@ def util_replace_files(obj, path, extract, tst_uid):
         obj['auth'] = _auth(obj)
 
     click.echo("Listing old files...")
-    old_fle_list = _file_list(obj['url'], obj['auth'], tst_uid=tst_uid)
+    old_fle_list = client.file_list(obj['url'], obj['auth'], tst_uid=tst_uid)
     click.echo("Old files:\n {}".format(old_fle_list))
 
     click.echo("Removing old files...")
-    rem_fle_list = _test_file_remove(obj['url'], obj['auth'], tst_uid, old_fle_list)
+    rem_fle_list = client.test_file_remove(obj['url'], obj['auth'], tst_uid, old_fle_list)
     click.echo("Attached files:\n {}".format(rem_fle_list))
 
     click.echo("Deleting old files...")
     for fle_uid in old_fle_list:
         click.echo("Deleting file '{}'...".format(fle_uid))
-        fle = _file_delete(obj['url'], obj['auth'], fle_uid)
+        fle = client.file_delete(obj['url'], obj['auth'], fle_uid)
 
     click.echo("Creating new files...")
-    new_fle_list = _file_create(obj['url'], obj['auth'], path, extract)
+    new_fle_list = client.file_create(obj['url'], obj['auth'], path, extract)
     click.echo("New files:\n {}".format(new_fle_list))
 
     click.echo("Attaching files...")
-    tst_fle_list = _test_file_add(obj['url'], obj['auth'], tst_uid, new_fle_list)
+    tst_fle_list = client.test_file_add(obj['url'], obj['auth'], tst_uid, new_fle_list)
     click.echo("Attached files:\n {}".format(tst_fle_list))
 
 @util.command(name='token-show')
@@ -342,21 +241,21 @@ def setup_assignment(obj, asn_name, env, tst_name, tester, maxscore, path, extra
         obj['auth'] = _auth(obj)
 
     click.echo("Creating assignment...")
-    asn_list = _assignment_create(obj['url'], obj['auth'], asn_name, env)
+    asn_list = client.assignment_create(obj['url'], obj['auth'], asn_name, env)
     click.echo("Created assignments:\n {}".format(asn_list))
     asn_uid = asn_list[0]
 
     click.echo("Creating test...")
-    tst_list = _assignment_test_create(obj['url'], obj['auth'], asn_uid, tst_name, tester, maxscore)
+    tst_list = client.assignment_test_create(obj['url'], obj['auth'], asn_uid, tst_name, tester, maxscore)
     click.echo("Created tests:\n {}".format(tst_list))
     tst_uid = tst_list[0]
 
     click.echo("Creating files...")
-    new_fle_list = _file_create(obj['url'], obj['auth'], path, extract)
+    new_fle_list = client.file_create(obj['url'], obj['auth'], path, extract)
     click.echo("Created files:\n {}".format(new_fle_list))
 
     click.echo("Attaching files...")
-    tst_fle_list = _test_file_add(obj['url'], obj['auth'], tst_uid, new_fle_list)
+    tst_fle_list = client.test_file_add(obj['url'], obj['auth'], tst_uid, new_fle_list)
     click.echo("Attached files:\n {}".format(tst_fle_list))
 
 if __name__ == '__main__':
---
+++ client.py
@@ -0,0 +1,113 @@
+# Andy Sayler
+# COG API Client
+# v2 API
+# September 2015
+
+import sys
+import json
+
+import requests
+
+_EP_ASSIGNMENTS = 'assignments'
+_KEY_ASSIGNMENTS = 'assignments'
+_EP_TESTS = 'tests'
+_KEY_TESTS = 'tests'
+_EP_FILES = 'files'
+_KEY_FILES = 'files'
+
+def assignment_create(url, auth, asn_name, asn_env):
+
+    endpoint = "{:s}/{:s}/".format(url, _EP_ASSIGNMENTS)
+    d = {'name': asn_name, 'env': asn_env}
+    dj = json.dumps(d)
+    r = requests.post(endpoint, auth=auth, data=dj)
+    r.raise_for_status()
+    asn_list = r.json()[_KEY_ASSIGNMENTS]
+    return asn_list
+
+def assignment_list(url, auth):
+
+    endpoint = "{:s}/{:s}/".format(url, _EP_ASSIGNMENTS)
+    r = requests.get(endpoint, auth=auth)
+    r.raise_for_status()
+    asn_list = r.json()[_KEY_ASSIGNMENTS]
+    return asn_list
+
+def assignment_show(url, auth, uid):
+
+    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_ASSIGNMENTS, uid)
+    r = requests.get(endpoint, auth=auth)
+    r.raise_for_status()
+    asn = r.json()[uid]
+    return asn
+
+def assignment_test_create(url, auth, asn_uid, tst_name, tst_tester, tst_maxscore):
+
+    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_ASSIGNMENTS, asn_uid, _EP_TESTS)
+    d = {"name": tst_name, "tester": tst_tester, "maxscore": tst_maxscore}
+    dj = json.dumps(d)
+    r = requests.post(endpoint, auth=auth, data=dj)
+    r.raise_for_status()
+    tst_list = r.json()[_KEY_TESTS]
+    return tst_list
+
+def test_file_add(url, auth, tst_uid, fle_uids):
+
+    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
+    d = {_KEY_FILES: fle_uids}
+    dj = json.dumps(d)
+    r = requests.put(endpoint, auth=auth, data=dj)
+    r.raise_for_status()
+    fle_list = r.json()[_KEY_FILES]
+    return fle_list
+
+def test_file_remove(url, auth, tst_uid, fle_uids):
+
+    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
+    d = {_KEY_FILES: fle_uids}
+    dj = json.dumps(d)
+    r = requests.delete(endpoint, auth=auth, data=dj)
+    r.raise_for_status()
+    fle_list = r.json()[_KEY_FILES]
+    return fle_list
+
+def file_create(url, auth, path, extract=False):
+
+    if extract:
+        key = 'extract'
+    else:
+        key = 'file'
+
+    endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
+    files = {key: path}
+    r = requests.post(endpoint, files=files, auth=auth)
+    r.raise_for_status()
+    fle_list = r.json()[_KEY_FILES]
+    return fle_list
+
+def file_list(url, auth, tst_uid=None):
+
+    if tst_uid:
+        endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
+    else:
+        endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
+    r = requests.get(endpoint, auth=auth)
+    r.raise_for_status()
+    fle_list = r.json()[_KEY_FILES]
+    return fle_list
+
+def file_show(url, auth, uid):
+
+    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
+    r = requests.get(endpoint, auth=auth)
+    r.raise_for_status()
+    fle = r.json()[uid]
+    return fle
+
+def file_delete(url, auth, uid):
+
+    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
+    r = requests.delete(endpoint, auth=auth)
+    r.raise_for_status()
+    fle = r.json()[uid]
+    return fle
