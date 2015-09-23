082a6bf31cd386adca80d1189e4f52e0627bd66d
Andy Sayler <andy.sayler@gmail.com>
2015-09-22 21:31:52 -0600

    Commented out old functions

--- client.py
+++ client.py
@@ -127,73 +127,73 @@ class Assignments(object):
         obj = res[uid]
         return obj
 
-def assignment_test_create(url, auth, asn_uid, tst_name, tst_tester, tst_maxscore):
+# def assignment_test_create(url, auth, asn_uid, tst_name, tst_tester, tst_maxscore):
 
-    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_ASSIGNMENTS, asn_uid, _EP_TESTS)
-    d = {"name": tst_name, "tester": tst_tester, "maxscore": tst_maxscore}
-    dj = json.dumps(d)
-    r = requests.post(endpoint, auth=auth, data=dj)
-    r.raise_for_status()
-    tst_list = r.json()[_KEY_TESTS]
-    return tst_list
+#     endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_ASSIGNMENTS, asn_uid, _EP_TESTS)
+#     d = {"name": tst_name, "tester": tst_tester, "maxscore": tst_maxscore}
+#     dj = json.dumps(d)
+#     r = requests.post(endpoint, auth=auth, data=dj)
+#     r.raise_for_status()
+#     tst_list = r.json()[_KEY_TESTS]
+#     return tst_list
 
-def test_file_add(url, auth, tst_uid, fle_uids):
+# def test_file_add(url, auth, tst_uid, fle_uids):
 
-    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
-    d = {_KEY_FILES: fle_uids}
-    dj = json.dumps(d)
-    r = requests.put(endpoint, auth=auth, data=dj)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
+#     endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
+#     d = {_KEY_FILES: fle_uids}
+#     dj = json.dumps(d)
+#     r = requests.put(endpoint, auth=auth, data=dj)
+#     r.raise_for_status()
+#     fle_list = r.json()[_KEY_FILES]
+#     return fle_list
 
-def test_file_remove(url, auth, tst_uid, fle_uids):
+# def test_file_remove(url, auth, tst_uid, fle_uids):
 
-    endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
-    d = {_KEY_FILES: fle_uids}
-    dj = json.dumps(d)
-    r = requests.delete(endpoint, auth=auth, data=dj)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
+#     endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
+#     d = {_KEY_FILES: fle_uids}
+#     dj = json.dumps(d)
+#     r = requests.delete(endpoint, auth=auth, data=dj)
+#     r.raise_for_status()
+#     fle_list = r.json()[_KEY_FILES]
+#     return fle_list
 
-def file_create(url, auth, path, extract=False):
+# def file_create(url, auth, path, extract=False):
 
-    if extract:
-        key = 'extract'
-    else:
-        key = 'file'
+#     if extract:
+#         key = 'extract'
+#     else:
+#         key = 'file'
 
-    endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
-    files = {key: path}
-    r = requests.post(endpoint, files=files, auth=auth)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
+#     endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
+#     files = {key: path}
+#     r = requests.post(endpoint, files=files, auth=auth)
+#     r.raise_for_status()
+#     fle_list = r.json()[_KEY_FILES]
+#     return fle_list
 
-def file_list(url, auth, tst_uid=None):
+# def file_list(url, auth, tst_uid=None):
 
-    if tst_uid:
-        endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
-    else:
-        endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
-    r = requests.get(endpoint, auth=auth)
-    r.raise_for_status()
-    fle_list = r.json()[_KEY_FILES]
-    return fle_list
+#     if tst_uid:
+#         endpoint = "{:s}/{:s}/{:s}/{:s}/".format(url, _EP_TESTS, tst_uid, _EP_FILES)
+#     else:
+#         endpoint = "{:s}/{:s}/".format(url, _EP_FILES)
+#     r = requests.get(endpoint, auth=auth)
+#     r.raise_for_status()
+#     fle_list = r.json()[_KEY_FILES]
+#     return fle_list
 
-def file_show(url, auth, uid):
+# def file_show(url, auth, uid):
 
-    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
-    r = requests.get(endpoint, auth=auth)
-    r.raise_for_status()
-    fle = r.json()[uid]
-    return fle
+#     endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
+#     r = requests.get(endpoint, auth=auth)
+#     r.raise_for_status()
+#     fle = r.json()[uid]
+#     return fle
 
-def file_delete(url, auth, uid):
+# def file_delete(url, auth, uid):
 
-    endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
-    r = requests.delete(endpoint, auth=auth)
-    r.raise_for_status()
-    fle = r.json()[uid]
-    return fle
+#     endpoint = "{:s}/{:s}/{:s}/".format(url, _EP_FILES, uid)
+#     r = requests.delete(endpoint, auth=auth)
+#     r.raise_for_status()
+#     fle = r.json()[uid]
+#     return fle
