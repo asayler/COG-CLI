47424cbb8d95dd1bf61300d5e98283de58c6ef59
Andy Sayler <andy.sayler@gmail.com>
2015-09-22 21:05:53 -0600

    Commented out unimplmented functions

--- cli.py
+++ cli.py
@@ -70,138 +70,165 @@ def assignment_list(obj):
     asn_list = obj['assignments'].list()
     click.echo("{}".format(asn_list))
 
-    click.echo("Creating test...")
-    tst_list = client.assignment_test_create(obj['url'], obj['auth'], asn_uid, name, tester, maxscore)
-    click.echo("Created tests:\n {}".format(tst_list))
+# @assignment.command(name='show')
+# @click.option('--uid', default=None, prompt=True, help='Assignment UUID')
+# @click.pass_obj
+# def assignment_show(obj, uid):
 
-@cli.group()
-def file():
-    pass
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-@file.command(name='create')
-@click.option('--path', default=None, prompt=True, type=click.File('rb'), help='File Path')
-@click.option('--extract', is_flag=True, help='Control whether file is extracted')
-@click.pass_obj
-def file_create(obj, path, extract):
+#     click.echo("Showing assignment...")
+#     asn = client.assignment_show(obj['url'], obj['connection'], uid)
+#     click.echo("Assignment '{}':\n {}".format(uid, asn))
 
-    if not obj['auth']:
-        obj['auth'] = _auth(obj)
+# @cli.group()
+# def test():
+#     pass
 
-    click.echo("Creating file...")
-    fle_list = client.file_create(obj['url'], obj['auth'], path, extract)
-    click.echo("Files:\n {}".format(fle_list))
+# @test.command(name='create')
+# @click.option('--asn_uid', default=None, prompt=True, help='Assignment UUID')
+# @click.option('--name', default=None, prompt=True, help='Test Name')
+# @click.option('--tester', default=None, prompt=True, help='Test Module')
+# @click.option('--maxscore', default=None, prompt=True, help='Max Score')
+# @click.pass_obj
+# def test_create(obj, asn_uid, name, tester, maxscore):
 
-@file.command(name='list')
-@click.option('--tst_uid', default=None, help='Only Show Files from Test with UUID tst_uid')
-@click.pass_obj
-def file_list(obj, tst_uid):
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-    if not obj['auth']:
-        obj['auth'] = _auth(obj)
+#     click.echo("Creating test...")
+#     tst_list = client.assignment_test_create(obj['url'], obj['connection'], asn_uid, name, tester, maxscore)
+#     click.echo("Created tests:\n {}".format(tst_list))
 
-    click.echo("Listing files...")
-    fle_list = client.file_list(obj['url'], obj['auth'], tst_uid=tst_uid)
-    click.echo("Files:\n {}".format(fle_list))
+# @cli.group()
+# def file():
+#     pass
 
-@file.command(name='show')
-@click.option('--uid', default=None, prompt=True, help='File UUID')
-@click.pass_obj
-def file_show(obj, uid):
+# @file.command(name='create')
+# @click.option('--path', default=None, prompt=True, type=click.File('rb'), help='File Path')
+# @click.option('--extract', is_flag=True, help='Control whether file is extracted')
+# @click.pass_obj
+# def file_create(obj, path, extract):
 
-    if not obj['auth']:
-        obj['auth'] = _auth(obj)
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-    click.echo("Showing file...")
-    fle = client.file_show(obj['url'], obj['auth'], uid)
-    click.echo("File '{}':\n {}".format(uid, fle))
+#     click.echo("Creating file...")
+#     fle_list = client.file_create(obj['url'], obj['connection'], path, extract)
+#     click.echo("Files:\n {}".format(fle_list))
 
-@file.command(name='show')
-@click.option('--uid', default=None, prompt=True, help='File UUID')
-@click.pass_obj
-def file_delete(obj, uid):
+# @file.command(name='list')
+# @click.option('--tst_uid', default=None, help='Only Show Files from Test with UUID tst_uid')
+# @click.pass_obj
+# def file_list(obj, tst_uid):
 
-    if not obj['auth']:
-        obj['auth'] = _auth(obj)
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-    click.echo("Deleting file...")
-    fle = client.file_delete(obj['url'], obj['auth'], uid)
-    click.echo("Deleted file '{}':\n {}".format(uid, fle))
+#     click.echo("Listing files...")
+#     fle_list = client.file_list(obj['url'], obj['connection'], tst_uid=tst_uid)
+#     click.echo("Files:\n {}".format(fle_list))
 
-@cli.group()
-def util():
-    pass
+# @file.command(name='show')
+# @click.option('--uid', default=None, prompt=True, help='File UUID')
+# @click.pass_obj
+# def file_show(obj, uid):
 
-@util.command(name='replace-files')
-@click.option('--path', default=None, prompt=True, type=click.File('rb'), help='File Path')
-@click.option('--extract', is_flag=True, help='Control whether file is extracted')
-@click.option('--tst_uid', default=None, prompt=True, help='Test UUID')
-@click.pass_obj
-def util_replace_files(obj, path, extract, tst_uid):
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-    if not obj['auth']:
-        obj['auth'] = _auth(obj)
+#     click.echo("Showing file...")
+#     fle = client.file_show(obj['url'], obj['connection'], uid)
+#     click.echo("File '{}':\n {}".format(uid, fle))
 
-    click.echo("Listing old files...")
-    old_fle_list = client.file_list(obj['url'], obj['auth'], tst_uid=tst_uid)
-    click.echo("Old files:\n {}".format(old_fle_list))
+# @file.command(name='show')
+# @click.option('--uid', default=None, prompt=True, help='File UUID')
+# @click.pass_obj
+# def file_delete(obj, uid):
 
-    click.echo("Removing old files...")
-    rem_fle_list = client.test_file_remove(obj['url'], obj['auth'], tst_uid, old_fle_list)
-    click.echo("Attached files:\n {}".format(rem_fle_list))
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-    click.echo("Deleting old files...")
-    for fle_uid in old_fle_list:
-        click.echo("Deleting file '{}'...".format(fle_uid))
-        fle = client.file_delete(obj['url'], obj['auth'], fle_uid)
+#     click.echo("Deleting file...")
+#     fle = client.file_delete(obj['url'], obj['connection'], uid)
+#     click.echo("Deleted file '{}':\n {}".format(uid, fle))
 
-    click.echo("Creating new files...")
-    new_fle_list = client.file_create(obj['url'], obj['auth'], path, extract)
-    click.echo("New files:\n {}".format(new_fle_list))
+# @cli.group()
+# def util():
+#     pass
 
-    click.echo("Attaching files...")
-    tst_fle_list = client.test_file_add(obj['url'], obj['auth'], tst_uid, new_fle_list)
-    click.echo("Attached files:\n {}".format(tst_fle_list))
+# @util.command(name='replace-files')
+# @click.option('--path', default=None, prompt=True, type=click.File('rb'), help='File Path')
+# @click.option('--extract', is_flag=True, help='Control whether file is extracted')
+# @click.option('--tst_uid', default=None, prompt=True, help='Test UUID')
+# @click.pass_obj
+# def util_replace_files(obj, path, extract, tst_uid):
 
-@util.command(name='token-show')
-@click.pass_obj
-def util_token_show(obj):
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-    if not obj['auth']:
-        obj['auth'] = _auth(obj)
+#     click.echo("Listing old files...")
+#     old_fle_list = client.file_list(obj['url'], obj['connection'], tst_uid=tst_uid)
+#     click.echo("Old files:\n {}".format(old_fle_list))
 
-    click.echo("{} Token: '{}'".format(obj['username'], obj['token']))
+#     click.echo("Removing old files...")
+#     rem_fle_list = client.test_file_remove(obj['url'], obj['connection'], tst_uid, old_fle_list)
+#     click.echo("Attached files:\n {}".format(rem_fle_list))
 
-@util.command(name='setup-assignment')
-@click.option('--asn_name', default=None, prompt=True, help='Assignment Name')
-@click.option('--env', default=None, prompt=True, help='Assignemnt Environment')
-@click.option('--tst_name', default=None, prompt=True, help='Test Name')
-@click.option('--tester', default=None, prompt=True, help='Test Module')
-@click.option('--maxscore', default=None, prompt=True, help='Max Score')
-@click.option('--path', default=None, prompt=True, type=click.File('rb'), help='File Path')
-@click.option('--extract', is_flag=True, help='Control whether file is extracted')
-@click.pass_obj
-def setup_assignment(obj, asn_name, env, tst_name, tester, maxscore, path, extract):
+#     click.echo("Deleting old files...")
+#     for fle_uid in old_fle_list:
+#         click.echo("Deleting file '{}'...".format(fle_uid))
+#         fle = client.file_delete(obj['url'], obj['connection'], fle_uid)
 
-    if not obj['auth']:
-        obj['auth'] = _auth(obj)
+#     click.echo("Creating new files...")
+#     new_fle_list = client.file_create(obj['url'], obj['connection'], path, extract)
+#     click.echo("New files:\n {}".format(new_fle_list))
 
-    click.echo("Creating assignment...")
-    asn_list = client.assignment_create(obj['url'], obj['auth'], asn_name, env)
-    click.echo("Created assignments:\n {}".format(asn_list))
-    asn_uid = asn_list[0]
+#     click.echo("Attaching files...")
+#     tst_fle_list = client.test_file_add(obj['url'], obj['connection'], tst_uid, new_fle_list)
+#     click.echo("Attached files:\n {}".format(tst_fle_list))
 
-    click.echo("Creating test...")
-    tst_list = client.assignment_test_create(obj['url'], obj['auth'], asn_uid, tst_name, tester, maxscore)
-    click.echo("Created tests:\n {}".format(tst_list))
-    tst_uid = tst_list[0]
+# @util.command(name='token-show')
+# @click.pass_obj
+# def util_token_show(obj):
 
-    click.echo("Creating files...")
-    new_fle_list = client.file_create(obj['url'], obj['auth'], path, extract)
-    click.echo("Created files:\n {}".format(new_fle_list))
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
 
-    click.echo("Attaching files...")
-    tst_fle_list = client.test_file_add(obj['url'], obj['auth'], tst_uid, new_fle_list)
-    click.echo("Attached files:\n {}".format(tst_fle_list))
+#     click.echo("{} Token: '{}'".format(obj['username'], obj['token']))
+
+# @util.command(name='setup-assignment')
+# @click.option('--asn_name', default=None, prompt=True, help='Assignment Name')
+# @click.option('--env', default=None, prompt=True, help='Assignemnt Environment')
+# @click.option('--tst_name', default=None, prompt=True, help='Test Name')
+# @click.option('--tester', default=None, prompt=True, help='Test Module')
+# @click.option('--maxscore', default=None, prompt=True, help='Max Score')
+# @click.option('--path', default=None, prompt=True, type=click.File('rb'), help='File Path')
+# @click.option('--extract', is_flag=True, help='Control whether file is extracted')
+# @click.pass_obj
+# def setup_assignment(obj, asn_name, env, tst_name, tester, maxscore, path, extract):
+
+#     if not obj['connection']:
+#         obj['connection'] = _connect(obj)
+
+#     click.echo("Creating assignment...")
+#     asn_list = client.assignment_create(obj['url'], obj['connection'], asn_name, env)
+#     click.echo("Created assignments:\n {}".format(asn_list))
+#     asn_uid = asn_list[0]
+
+#     click.echo("Creating test...")
+#     tst_list = client.assignment_test_create(obj['url'], obj['connection'], asn_uid, tst_name, tester, maxscore)
+#     click.echo("Created tests:\n {}".format(tst_list))
+#     tst_uid = tst_list[0]
+
+#     click.echo("Creating files...")
+#     new_fle_list = client.file_create(obj['url'], obj['connection'], path, extract)
+#     click.echo("Created files:\n {}".format(new_fle_list))
+
+#     click.echo("Attaching files...")
+#     tst_fle_list = client.test_file_add(obj['url'], obj['connection'], tst_uid, new_fle_list)
+#     click.echo("Attached files:\n {}".format(tst_fle_list))
 
 if __name__ == '__main__':
     sys.exit(cli())
